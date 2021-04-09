77dbc4f3956a7dfb17ee92de32957468e01a5ddc
Balázs Szeidl <szeidl.balazs@hotmail.com>
2021-04-09 23:53:19 +0200

    Show more works and button text changes from show more to show less, but reverse still to do.

--- src/Components/Laptop.jsx
+++ src/Components/Laptop.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const Laptop = (props) => {
+
+	const [showMore, setShowMore] = useState(false);
+	const [buttonState, setButtonState] = useState('Show more');
+
+	
 
 	return (
 		<div className="laptop-container">
 			<div>{props.name}</div>
-			<button>Show more</button>
-			<div>{props.brand}</div>
-			<div>{props.weigth}</div>
+			<button onClick={() => {setShowMore(true); setButtonState('Show less');}}>{buttonState}</button>
+
+				{showMore ? <div>{props.brand}</div> : false}
+				{showMore ? <div>{props.weigth}</div> : false}
+
 		</div>
 	)
 };
 
 export default Laptop;
+
+/*	<button onClick={() => {setShowMore(true); setButtonState('Show less');}}>{buttonState}</button> */
